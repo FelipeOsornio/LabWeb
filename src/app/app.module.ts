32981488f37e6144ce23123ec3338d8959037660
valentin <A01371084@itesm.mx>
2020-08-31 18:51:30 -0500

    galeriade imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './componentes/menu.component';
 import { AcordeonComponent } from './componentes/acordeon/acordeon.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    AcordeonComponent
+    AcordeonComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule
