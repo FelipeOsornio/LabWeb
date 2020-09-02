4a69b14c83214f67365a295c44e967e1175515e7
FelipeOsornio <felipe.osrn@gmail.com>
2020-09-01 20:33:43 -0500

    singlepageapp

---
+++ singlepageapp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EncabezadoComponent} from './componentes/comunes/encabezado/encabezado.component';
+import {PrincipalComponent} from './componentes/principal/principal.component';
+import {AcercadeComponent} from './componentes/acercade/acercade.component';
+import {ConsolasComponent} from './componentes/consolas/consolas.component';
+import {APP_ROUTING} from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    PrincipalComponent,
+    AcercadeComponent,
+    ConsolasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTING
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {
+}
