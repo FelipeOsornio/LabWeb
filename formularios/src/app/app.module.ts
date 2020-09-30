21f2c1c3b3e98260ef85b9089f3237775526cdba
Felipe Osornio <deiv@SkDv.local>
2020-09-29 20:29:39 -0500

    práctica formularios

--- formularios/src/app/app.module.ts
+++ formularios/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
+
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
